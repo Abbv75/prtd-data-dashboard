0d1d1a5b5b0a0a192d1bd75c004b5e4db7d9f3c2
abbv75 <bore.younous59@gmail.com>
2025-04-10 16:01:21 +0000

    `Refactor Routes and Accueil components with minor styling changes`

--- src/pages/Accueil/index.tsx
+++ src/pages/Accueil/index.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { Stack, Tab, tabClasses, TabList, Tabs } from '@mui/joy'
+import { green, orange, red } from '@mui/material/colors'
 
 const index = () => {
     return (
-        <div>index</div>
+        <Stack>
+            <Tabs>
+                <TabList
+                    tabFlex={"auto"}
+                    sx={{
+                        bgcolor: orange[500],
+                        fontWeight: 700,
+                        [`& .${tabClasses.root}`]: {
+                            [`&[aria-selected="true"]`]: {
+                                color: 'white',
+                                bgcolor: green[800],
+                            },
+                        },
+                    }}
+                >
+                    <Tab disableIndicator value="1">One</Tab>
+                    <Tab disableIndicator value="2">One</Tab>
+                    <Tab disableIndicator value="3">One</Tab>
+                </TabList>
+            </Tabs>
+        </Stack>
     )
 }
 
