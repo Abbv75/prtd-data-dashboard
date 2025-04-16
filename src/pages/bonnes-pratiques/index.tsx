b8a4785414ab7f74e131720363354bbd17d43897
abbv75 <bore.younous59@gmail.com>
2025-04-16 11:56:40 +0000

    `Added search bar and add button to BonnesPratiques page`

--- src/pages/bonnes-pratiques/index.tsx
+++ src/pages/bonnes-pratiques/index.tsx
@@ -1,6 +1,23 @@
+import { faPlusCircle, faSearch } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Button, Card, Input, Stack } from "@mui/joy"
+
 const BonnesPratiques = () => {
     return (
-        <div>BonnesPratiques</div>
+        <Stack>
+            <Card
+                sx={{ borderRadius: 0, flexDirection: "row", p: 0.5, gap: 1 }}
+            >
+                <Input
+                    fullWidth
+                    placeholder="Saisissez pour rechercher"
+                    startDecorator={<FontAwesomeIcon icon={faSearch} />}
+                />
+                <Button
+                    endDecorator={<FontAwesomeIcon icon={faPlusCircle} />}
+                >Ajouter</Button>
+            </Card>
+        </Stack>
     )
 }
 
